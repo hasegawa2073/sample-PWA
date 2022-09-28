4770764d360898bc5b46ec7fa1cc6689c3d5398c
hasegawa2073 <hasetatsu2001@icloud.com>
2022-09-28 17:23:25 +0900

    fix: キャッシュにないリソースをネットワークからリクエストするようにした

--- sw.js
+++ sw.js
@@ -2,12 +2,30 @@ const addResourcesToCache = async (resources) => {
   const cache = await caches.open('v1');
   await cache.addAll(resources);
 };
-const cacheFirst = async (request) => {
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
-    return requestFromCache;
+    return responseFromCache;
   }
-  return fetch(request);
+  try {
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
 };
 
 self.addEventListener('install', (e) => {
@@ -29,5 +47,5 @@ self.addEventListener('activate', (e) => {
   console.log(`activate: ${e}`);
 });
 self.addEventListener('fetch', (e) => {
-  e.respondWith(cacheFirst(e.request));
+  e.respondWith(cacheFirst({ request: e.request, fallbackUrl: '' }));
 });
